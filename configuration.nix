4e9ea8ddadc2313d3bf568a11bbc571236e5fa55
TXG0Fk3 <137466333+TXG0Fk3@users.noreply.github.com>
2025-11-10 02:57:34 -0300

    feat: refine GNOME environment with custom apps and extensions

--- configuration.nix
+++ configuration.nix
@@ -33,10 +33,14 @@
   services.xserver.xkb.layout = "br";
 
   # UI
-  services.xserver = {
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+  services = {
+    xserver = {
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+    gnome.core-apps.enable = false;
   };
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
 
   # Sound.
   security.rtkit.enable = true;
@@ -61,14 +65,30 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     packages = with pkgs; [
-      tree
+      gnome-text-editor
+      baobab
+      decibels
+      loupe
+      showtime
+
+      morewaita-icon-theme
     ];
   };
 
   # System Packages.
   environment.systemPackages = with pkgs; [
-    gnome-system-monitor
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.logo-menu
+    gnomeExtensions.space-bar
+    gnomeExtensions.appindicator
+    gnomeExtensions.window-title-is-back
+    gnomeExtensions.rounded-window-corners-reborn
+    
+    gnome-console
     gnome-disk-utility
+    gnome-tweaks
+    mission-center
+    nautilus
 
     vim
     btop
