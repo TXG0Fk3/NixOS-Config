edc56f505b0def499d742ea0b88b29a8d6b49b70
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-13 16:02:38 -0300

    feat: add Hydra host configuration

--- flake.nix
+++ flake.nix
@@ -17,27 +17,30 @@
     home-modules = ./home-manager/modules;
   in
   {  
-    # Orion
-    nixosConfigurations.Orion = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {
-        inherit inputs;
-        inherit system-modules;
-      };
-      modules = [
-        ./system/hosts/orion
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {
-              inherit inputs;
-              inherit home-modules;
+    nixosConfigurations = {
+      # Orion
+      Orion = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs system-modules; };
+        modules = [
+          ./system/hosts/orion
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs home-modules; };
+              users.TXG0Fk3 = import ./home-manager/users/txg0fk3.nix;
             };
-            users.TXG0Fk3 = import ./home-manager/users/txg0fk3.nix;
-          };
-        }
-      ];
+          }
+        ];
+      };
+
+      # Hydra
+      Hydra = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs system-modules; };
+        modules = [ ./system/hosts/hydra ];
+      };
     };
   };
 }
---
+++ system/hosts/hydra/default.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, system-modules, ... }:
+
+{
+  imports = [
+    #./hardware-configuration.nix
+    system-modules
+  ];
+
+  # Bootloader and Kernel
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    }; 
+    tmp.useTmpfs = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  # Network
+  networking = {
+    hostName = "Hydra";
+    firewall = {
+      enable =  true;
+      allowedTCPPorts = [ 22 80 443 ];
+    };
+  };
+
+  # Users.
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "podman" ];
+  };
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    superfile
+    podman-compose
+  ];
+
+  # Containers
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  # Services
+  services = {
+    openssh.enable = true;
+    fail2ban.enable = true;
+  };
+
+  system.stateVersion = "25.05";
+}
