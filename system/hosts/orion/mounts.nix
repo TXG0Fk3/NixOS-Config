d3f8dc329a49a5e620992815a0cf78b6b6585719
TXG0Fk3 <137466333+TXG0Fk3@users.noreply.github.com>
2025-12-23 15:09:15 -0300

    refactor(mounts): change disk references to labels

--- system/hosts/orion/mounts.nix
+++ system/hosts/orion/mounts.nix
@@ -3,13 +3,13 @@
 {
   # FileSystems
   fileSystems."/mnt/ssd" = {
-    device = "/dev/disk/by-uuid/7f04f633-d8ee-4767-b570-46b3af240565";
+    device = "/dev/disk/by-label/SSD";
     fsType = "f2fs";
     options = [ "noatime" "nodiratime" "discard" "lazytime" "compress_chksum" "x-gvfs-show" "rw" ];
   };
 
   fileSystems."/mnt/hdd" = {
-    device = "/dev/disk/by-uuid/927b9270-9063-4dc9-b678-6ecfd8aa6794";
+    device = "/dev/disk/by-label/HDD";
     fsType = "ext4";
     options = [ "relatime" "nofail" "x-gvfs-show" "rw" ];
   };
