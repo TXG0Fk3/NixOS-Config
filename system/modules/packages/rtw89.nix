148148b3f4ca7dc8b33df5a902fb04194954cdfc
TXG0Fk3 <leovertonx12@outlook.com>
2025-12-26 11:08:59 -0300

    fix(rtw89): correct hash

--- system/modules/packages/rtw89.nix
+++ system/modules/packages/rtw89.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     owner = "morrownr";
     repo = "rtw89";
     rev = "master";
-    hash = "sha256-7da080fc2fc75709364943c7ef2b39fa5abcef77=";
+    hash = "sha256-psl39LMyXZh4Not6qWSdanze19GX0G52QbtLM7cO8JA=";
   };
 
   nativeBuildInputs = [ bc nukeReferences ] ++ kernel.moduleBuildDependencies;
