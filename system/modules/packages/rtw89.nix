b42f24a7b4649b0bd9769c2e56164dd716576f84
TXG0Fk3 <leovertonx12@outlook.com>
2025-12-26 11:01:20 -0300

    feat(sys packages): add rtw89 driver

---
+++ system/modules/packages/rtw89.nix
@@ -0,0 +1,33 @@
+{ lib, stdenv, fetchFromGitHub, kernel, bc, nukeReferences }:
+
+stdenv.mkDerivation {
+  pname = "rtw89-morrownr";
+  version = "${kernel.version}";
+
+  src = fetchFromGitHub {
+    owner = "morrownr";
+    repo = "rtw89";
+    rev = "master";
+    hash = "sha256-7da080fc2fc75709364943c7ef2b39fa5abcef77=";
+  };
+
+  nativeBuildInputs = [ bc nukeReferences ] ++ kernel.moduleBuildDependencies;
+  hardeningDisable = [ "pic" ];
+
+  makeFlags = [
+    "KVER=${kernel.modDirVersion}"
+    "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtw89
+    cp *.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtw89
+    nuke-refs $out/lib/modules/*/kernel/drivers/net/wireless/realtek/rtw89/*.ko
+  '';
+
+  meta = with lib; {
+    description = "Realtek rtw89 WiFi driver with USB support (morrownr fork)";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+  };
+}
