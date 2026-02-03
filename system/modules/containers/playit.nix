f414bf30e80c4e56250f4a49b042f161aa712631
TXG0Fk3 <leovertonx12@outlook.com>
2026-02-03 13:32:56 -0300

    feat(containers): add playit

---
+++ system/modules/containers/playit.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.homelab.playit;
+in
+{
+  options.services.homelab.playit = {
+    enable = mkEnableOption "Activate the Playit.gg agent.";
+  };
+
+  config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d /var/lib/playit 0700 root root -"
+    ];
+
+    virtualisation.oci-containers.containers.playit = {
+      image = "ghcr.io/playit-cloud/playit-agent:0.16";
+      extraOptions = [ "--network=host" ];
+      environmentFiles = [
+        "/var/lib/playit/playit.env"
+      ];
+    };
+  };
+}
