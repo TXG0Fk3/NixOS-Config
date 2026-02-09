828a94e3773667c0b1c1ec55d65eddf755930df5
TXG0Fk3 <leovertonx12@outlook.com>
2026-02-09 16:01:36 -0300

    refactor(hydra mounts): rename STORAGE-WD to STORAGE-SM and add hdparm command

--- system/hosts/hydra/mounts.nix
+++ system/hosts/hydra/mounts.nix
@@ -3,8 +3,14 @@
 {
   # FileSystems
   fileSystems."/mnt/storage" = {
-    device = "/dev/disk/by-label/STORAGE-WD";
-    fsType = "ext4";
-    options = [ "noatime" "errors=remount-ro" "nofail" "rw" ];
+    device = "/dev/disk/by-label/STORAGE-SM";
+    fsType = "xfs";
+    options = [ "noatime" "nofail" ];
   };
+
+  environment.systemPackages = [ pkgs.hdparm ];
+
+  powerManagement.powerUpCommands = ''
+    ${pkgs.hdparm}/sbin/hdparm -B 254 -S 0 /dev/disk/by-id/ata-SAMSUNG_HM500JI_S227J56B710852
+  '';
 }
