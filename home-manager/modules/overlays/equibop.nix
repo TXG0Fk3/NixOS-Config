92d57c04895506bd2e52da39d43b27c89a5e4ee6
TXG0Fk3 <txg0fk3@proton.me>
2026-02-20 10:25:39 -0300

    feat(hm modules): update equibop to 3.1.9

--- home-manager/modules/overlays/equibop.nix
+++ home-manager/modules/overlays/equibop.nix
@@ -1,11 +1,11 @@
 final: prev: {
   equibop = prev.stdenv.mkDerivation rec {
     pname = "equibop";
-    version = "3.1.8";
+    version = "3.1.9";
 
     src = prev.fetchurl {
       url = "https://github.com/Equicord/Equibop/releases/download/v${version}/equibop-${version}.tar.gz";
-      sha256 = "58b91351b9c3044ada50e93ee47a57c2edfa5a48c90527e7a6370dc2387e81eb";
+      sha256 = "e344946af86a85c60bc4e7cc9efbfc3ac3fee7c41634bce1744bb41a6c0b469f";
     };
 
     nativeBuildInputs = [
