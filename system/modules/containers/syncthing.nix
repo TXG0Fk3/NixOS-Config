e82e2235c0a2506305701e389b721b50e3485217
TXG0Fk3 <txg0fk3@proton.me>
2026-03-11 15:00:03 -0300

    feat(sysmodules): add syncthing container

---
+++ system/modules/containers/syncthing.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.services.homelab.syncthing;
+in
+{
+  options.services.homelab.syncthing = {
+    enable = mkEnableOption "Activate the Syncthing container.";
+
+    user = mkOption {
+      type = types.str;
+      example = "admin";
+      description = "Owner of the cloudPath and config files.";
+    };
+
+    cloudPath = mkOption {
+      type = types.str;
+      example = "/mnt/cloud";
+      description = "Path to your files.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d /var/lib/containers/syncthing 0750 ${cfg.user} users -"
+    ];
+
+    virtualisation.oci-containers.containers.syncthing = {
+      image = "syncthing/syncthing";
+      user = "${toString config.users.users.${cfg.user}.uid}:${toString config.users.groups.users.gid}";
+      environment = {
+        TZ = "America/Maceio";
+      };
+      volumes = [
+        "/var/lib/containers/syncthing:/var/syncthing"
+        "${cfg.cloudPath}:/cloud"
+      ];
+      ports = [
+        "8384:8384"
+        "22000:22000/tcp"
+        "22000:22000/udp"
+      ];
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      8384
+      22000
+    ];
+    networking.firewall.allowedUDPPorts = [ 22000 ];
+  };
+}
