8ae1fd641266f0f511a4ae0b2c3025426a246778
TXG0Fk3 <txg0fk3@proton.me>
2026-03-19 01:36:28 -0300

    feat(hm/modules): create spotiflac package

---
+++ home-manager/modules/packages/spotiflac.nix
@@ -0,0 +1,60 @@
+{
+  pkgs,
+  lib,
+  fetchurl,
+  appimageTools,
+  makeDesktopItem,
+}:
+
+let
+  pname = "spotiflac";
+  version = "7.1.1";
+
+  src = fetchurl {
+    url = "https://github.com/afkarxyz/SpotiFLAC/releases/download/v${version}/SpotiFLAC.AppImage";
+    hash = "sha256-X28bE3JEBvKpsDQ1uHKCFTxAa3IK2l/pK2WCq+3oMPY=";
+  };
+
+  appImageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+
+  desktopItem = makeDesktopItem {
+    name = pname;
+    desktopName = "SpotiFLAC";
+    exec = "${pname} %U";
+    icon = pname;
+    comment = "Download Spotify tracks in FLAC from multiple sources";
+    categories = [
+      "Audio"
+      "Network"
+    ];
+    terminal = false;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraPkgs =
+    pkgs: with pkgs; [
+      ffmpeg
+      webkitgtk_4_1
+    ];
+
+  extraInstallCommands = ''
+    install -Dm444 ${desktopItem}/share/applications/*.desktop \
+      $out/share/applications/${pname}.desktop
+
+    install -Dm444 \
+      ${appImageContents}/usr/share/icons/hicolor/256x256/apps/${pname}.png \
+      $out/share/icons/hicolor/256x256/apps/${pname}.png
+  '';
+
+  meta = with lib; {
+    description = "Get Spotify tracks in true FLAC from Tidal, Qobuz & Amazon Music — no account required.";
+    homepage = "https://github.com/afkarxyz/SpotiFLAC";
+    license = licenses.mit;
+    platforms = platforms.linux;
+    mainProgram = "spotiflac";
+  };
+}
