7eedda791f47d73a22664e4f1685d9b8fcc0950f
TXG0Fk3 <txg0fk3@proton.me>
2026-04-07 01:42:53 -0300

    feat(phoenix): switch kernel to LTS

--- system/hosts/phoenix/default.nix
+++ system/hosts/phoenix/default.nix
@@ -19,7 +19,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
   };
 
   # Swap
