debff272fac6fd4876d0a8010af2d82e707d8dbf
TXG0Fk3 <txg0fk3@proton.me>
2026-04-27 03:13:11 -0300

    fix(hm-modules/bottles): workaround openldap build failure
    See: NixOS/nixpkgs#513245

--- home-manager/modules/bottles.nix
+++ home-manager/modules/bottles.nix
@@ -4,6 +4,30 @@
   home.packages = with pkgs; [
     (bottles.override {
       removeWarningPopup = true;
+
+      # Workaround for openldap-2.6.13 build failure
+      # "https://github.com/NixOS/nixpkgs/issues/513245" issuecomment-4319854191
+      # Intercept buildFHSEnv to modify target packages
+      buildFHSEnv =
+        args:
+        pkgs.buildFHSEnv (
+          args
+          // {
+            multiPkgs =
+              envPkgs:
+              let
+                # Fetch original package list
+                originalPkgs = args.multiPkgs envPkgs;
+
+                # Disable tests for openldap
+                customLdap = envPkgs.openldap.overrideAttrs (_: {
+                  doCheck = false;
+                });
+              in
+              # Replace broken openldap with the custom one
+              builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+          }
+        );
     })
   ];
 }
