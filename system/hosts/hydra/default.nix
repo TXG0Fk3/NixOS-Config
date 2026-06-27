b8279768a87377f8918ecd85318546ab9cb8bc09
TXG0Fk3 <txg0fk3@proton.me>
2026-06-27 15:53:07 -0300

    refactor(hydra): pin fixed ipv4 address for enp3s0

--- system/hosts/hydra/default.nix
+++ system/hosts/hydra/default.nix
@@ -67,7 +67,7 @@
     };
 
     # Onboard NIC
-    interfaces.enp2s0.ipv4.addresses = [
+    interfaces.enp3s0.ipv4.addresses = [
       {
         address = "192.168.1.112";
         prefixLength = 24;
