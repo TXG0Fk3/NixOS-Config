d7107a04e1cada2c4237103e98825f2161a6f086
TXG0Fk3 <txg0fk3@proton.me>
2026-08-05 16:45:19 -0300

    refactor(sys-modules/xfce): set default and excluded packages

--- system/modules/ui/xfce.nix
+++ system/modules/ui/xfce.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+  # https://github.com/NixOS/nixpkgs/blob/8c91a71d13451abc40eb9dae8910f972f979852f/nixos/modules/services/x11/desktop-managers/xfce.nix
   services.displayManager.ly = {
     enable = true;
     x11Support = false;
@@ -11,4 +12,22 @@
     enableWaylandSession = true;
     waylandSessionCompositor = "labwc --startup";
   };
+
+  environment.systemPackages = with pkgs; [
+    # XFCE Essentials
+    garcon
+    xfce4-whiskermenu-plugin
+    xfconf
+
+    # Programs
+    gnome-disk-utility
+    mousepad
+    thunar
+  ];
+
+  environment.xfce.excludePackages = with pkgs.xfce; [
+    hicolor-icon-theme
+    tango-icon-theme
+    xfce4-icon-theme
+  ];
 }
